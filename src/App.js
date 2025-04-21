a8a0335a459aa8131f4782a7cbceff8e89d0d846
Prachi <goyalprachi70150@gmail.com>
2025-04-22 00:39:40 +0530

    Project setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center items-center h-screen bg-blue-100">
+      <h1 className="text-3xl font-bold text-blue-600">Hello Prachi! 🎨 Tailwind is Working!</h1>
+      <h1>LESSGOOOOOOOOO!!!! </h1>
     </div>
   );
 }
