11c4d30e125404475eb494746d6907355d7f3426
Prachi <goyalprachi70150@gmail.com>
2025-06-07 17:29:58 +0530

    Initial commit: frontend

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 module.exports = {
-  content: [
-    "./src/**/*.{html,js,jsx,ts,tsx}",
-  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        satisfy: ['Satisfy', 'cursive'],
+        pacifico: ['Pacifico', 'cursive'],
+        dancing: ['Dancing Script', 'cursive'],
+        lobster: ['Lobster', 'cursive'],
+      },
+    },
   },
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   plugins: [],
 }
